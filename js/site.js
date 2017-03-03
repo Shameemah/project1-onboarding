883494fe63723c7e6b479f134aa1c939c9195e2a
Shameemah Fuseini-Codjoe <shameemahfuseini-codjoe@iMac.local>
2017-03-03 16:25:52 -0600

    Validated with web validator and eslint

--- js/site.js
+++ js/site.js
@@ -6,56 +6,56 @@ jQuery(function($) {
   $('html').addClass('hasjs');
   
   $('#email').on('keyup focus blur', function() {
-  	var currentValue = $(this).val();
-   	var validPattern = $(this).attr('pattern');
-   	if (currentValue.match(validPattern)) {
-   		$('#submit').addClass('active');
-   	} else {
-   		$('#submit').removeClass('active');
-   	}
+    var currentValue = $(this).val();
+    var validPattern = $(this).attr('pattern');
+    if (currentValue.match(validPattern)) {
+      $('#submit').addClass('active');
+    } else {
+      $('#submit').removeClass('active');
+    }
   });
   
   $('#email').on('focus', function() {
-  	$('#input-email label').addClass('email-active');
-  	$('input[type="email"]').addClass('email-active');
+    $('#input-email label').addClass('email-active');
+    $('input[type="email"]').addClass('email-active');
   });
   
   $('h3').on('click', function() {
-  	$('#input-email label').addClass('email-active');
-  	$('#email').focus();
+    $('#input-email label').addClass('email-active');
+    $('#email').focus();
   });
 
   $('#email, h3').on('blur', function() {
-		if($('#email').val().length == 0) {
-			$('#input-email label').removeClass('email-active');
-			$('input[type="email"]').removeClass('email-active');
-		}
+    if($('#email').val().length === 0) {
+      $('#input-email label').removeClass('email-active');
+      $('input[type="email"]').removeClass('email-active');
+    }
   });
   
   //Form submit action
   $( "#register-form" ).submit(function( event ) {
-  	event.preventDefault();
-  	$("#submit").remove();
-  	$("form").remove();
-	 	thankYouMessage();
-	});
-	
-	//reset content when form is reloaded
-	resetForms();
-	
-	//function to show thank you message to user
-	function thankYouMessage() {
-		var message = 'Thank You!';
-		var gratitude='<ul><li id="thankyou-message">You are officially a logophile :D</li></ul>';
-		$('h3').text(message);
-		$('h3').css('background-color', 'rgba(170, 102, 68, 0.6)');
-		$('#register').append(gratitude);
-		$('#thankyou-message').addClass('FontBulge');
-	}
-	
-	function resetForms() {
+    event.preventDefault();
+    $("#submit").remove();
+    $("form").remove();
+    thankYouMessage();
+  });
+  
+  //reset content when form is reloaded
+  resetForms();
+  
+  //function to show thank you message to user
+  function thankYouMessage() {
+    var message = 'Thank You!';
+    var gratitude='<ul><li id="thankyou-message">You are officially a logophile :D</li></ul>';
+    $('h3').text(message);
+    $('h3').css('background-color', 'rgba(170, 102, 68, 0.6)');
+    $('#register').append(gratitude);
+    $('#thankyou-message').addClass('FontBounce');
+  }
+  
+  function resetForms() {
     document.forms['register-form'].reset();
-	}
+  }
 });
 
 
